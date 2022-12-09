a213232ee5ef25b33f6423fe7d54449b40169eab
Leeyonghyeong <tont1990@gmail.com>
2022-12-09 10:19:20 +0900

    빌드용 api 주소 변경

--- config/axios.config.ts
+++ config/axios.config.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from 'axios'
 const instance: AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'http://3.38.51.90/api'
+      ? 'http://3.37.27.220/api'
       : 'http://192.168.0.22:3001/api',
 })
 
